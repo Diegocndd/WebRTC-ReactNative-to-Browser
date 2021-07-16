d6e044802a2f04050bd83a7d1444b39077f07ad4
unknown <diego.candido@dellead.com>
2021-07-16 08:32:56 -0300

    Added server

---
+++ server-js/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+let broadcaster;
+const port = 4000;
+
+const http = require("http");
+const server = http.createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+app.use(express.static(__dirname + "/public"));
+
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    console.log('socket id = ', socket.id);
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    console.log('watcher = ', socket.id);
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
